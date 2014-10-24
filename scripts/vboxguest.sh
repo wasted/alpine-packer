946546e370d109ae81b91f1672922657d06deaf1
Franz Bettag <franz@bett.ag>
2014-10-25 00:18:46 +0200

    Prepared for alpine with vbox additions. http://git.alpinelinux.org/cgit/aports/plain/testing/virtualbox-additions-grsec

--- scripts/vboxguest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#mkdir /tmp/virtualbox
-#VERSION=$(cat /home/vagrant/.vbox_version)
-#mount -o loop /home/vagrant/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
-#sh /tmp/virtualbox/VBoxLinuxAdditions.run
-#umount /tmp/virtualbox
-#rmdir /tmp/virtualbox
-#rm /home/vagrant/*.iso
